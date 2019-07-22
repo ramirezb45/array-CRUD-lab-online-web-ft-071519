86eb4297ea64458e9f8f04feb7766ecd1e324cc2
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-22 04:49:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   ["wow", "I", "am", "really", "learning"]
+  words =, "I", "am", "really", "learning"]
      add_element_to_end_of_array.push("arrays!")
 end
 
